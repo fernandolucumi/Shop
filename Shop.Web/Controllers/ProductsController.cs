f59749f26b1da31857d02a929dfe90b921cd520e
fernando lucumi cruz <fernandolucumi@gmail.com>
2019-09-11 21:19:59 -0500

    Adding Images for CRUD options

--- Shop.Web/Controllers/ProductsController.cs
+++ Shop.Web/Controllers/ProductsController.cs
@@ -6,6 +6,9 @@
     using Data;
     using Data.Entities;
     using Helpers;
+    using Models;
+    using System.IO;
+    using System;
 
     public class ProductsController : Controller
     {
@@ -51,16 +54,49 @@
         
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create(Product product)
+        public async Task<IActionResult> Create(ProductViewModel view)
         {
             if (ModelState.IsValid)
             {
+                var path = string.Empty;
+
+                if(view.ImageFile != null && view.ImageFile.Length > 0)
+                {
+                    path = Path.Combine(Directory.GetCurrentDirectory(), 
+                        "wwwroot\\images\\Products",
+                          view.ImageFile.FileName);
+
+                    using (var stream = new FileStream(path, FileMode.Create))
+                    {
+                        await view.ImageFile.CopyToAsync(stream);
+                    }
+
+                    path = $"~/images/Products/{view.ImageFile.FileName}";
+                }
+                var product = this.ToProduct(view, path);
+
                 //TODO: Change for the logged user
                 product.User = await this.userHelper.GetUserByEmailAsync("jzuluaga55@gmail.com");
                 await this.productRepository.CreateAsync(product);
                 return RedirectToAction(nameof(Index));
             }
-            return View(product);
+            return View(view);
+        }
+
+        private Product ToProduct(ProductViewModel view, string path)
+        {
+            return new Product
+            {
+                Id = view.Id,
+                ImageUrl = path,
+                IsAvailable = view.IsAvailable,
+                LastPurchase = view.LastPurchase,
+                LastSale = view.LastSale,
+                Name = view.Name,
+                Price = view.Price,
+                Stock = view.Stock,
+                User = view.User
+            };
         }
 
         // GET: Products/Edit/5
@@ -76,7 +112,24 @@
             {
                 return NotFound();
             }
-            return View(product);
+            var view = this.ToProductViewModel(product);
+            return View(view);
+        }
+
+        private ProductViewModel ToProductViewModel(Product product)
+        {
+            return new ProductViewModel
+            {
+                Id = product.Id,                
+                IsAvailable = product.IsAvailable,
+                LastPurchase = product.LastPurchase,
+                LastSale = product.LastSale,
+                ImageUrl = product.ImageUrl,
+                Name = product.Name,
+                Price = product.Price,
+                Stock = product.Stock,
+                User = product.User
+            };
         }
 
         // POST: Products/Edit/5
@@ -84,20 +137,38 @@
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task <IActionResult> Edit(Product product)
+        public async Task <IActionResult> Edit(ProductViewModel view)
         {
            
             if (ModelState.IsValid)
             {
                 try
                 {
+                    var path = view.ImageUrl;
+
+                    if (view.ImageFile != null && view.ImageFile.Length > 0)
+                    {
+                        path = Path.Combine(
+                            Directory.GetCurrentDirectory(),
+                            "wwwroot\\images\\Products",
+                              view.ImageFile.FileName);
+
+                        using (var stream = new FileStream(path, FileMode.Create))
+                        {
+                            await view.ImageFile.CopyToAsync(stream);
+                        }
+
+                        path = $"~/images/Products/{view.ImageFile.FileName}";
+                    }
+                    var product = this.ToProduct(view, path);
+
                     //TODO: Change for the logged user
                     product.User = await this.userHelper.GetUserByEmailAsync("jzuluaga55@gmail.com");
                     await this.productRepository.UpdateAsync(product);                    
                 }
                 catch (DbUpdateConcurrencyException)
                 {
-                    if (!await this.productRepository.ExistAsync(product.Id))
+                    if (!await this.productRepository.ExistAsync(view.Id))
                     {
                         return NotFound();
                     }
@@ -108,7 +179,7 @@
                 }
                 return RedirectToAction(nameof(Index));
             }
-            return View(product);
+            return View(view);
         }
 
         // GET: Products/Delete/5
---
+++ Shop.Web/Models/ProductViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace Shop.Web.Models
+{
+    using System.ComponentModel.DataAnnotations;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using Data.Entities;
+    using Microsoft.AspNetCore.Http;
+    public class ProductViewModel:Product
+    {
+        [Display(Name = "Image")]
+        public IFormFile ImageFile { get; set; }
+    }
+}
